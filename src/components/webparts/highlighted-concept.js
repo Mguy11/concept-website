fc36d419df5f342acac61f2949c21e9c8cac56f3
Martijn <mbankert145@gmail.com>
2020-04-19 22:29:26 +0200

    added all the seperation lines

---
+++ src/components/webparts/highlighted-concept.js
@@ -0,0 +1,24 @@
+import React,{ Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+export default class HighlightedConcept extends Component {
+    render() {
+        return(
+            <div className={this.props.class}>
+                <Row>
+                    <Col lg={{span:5, offset: 1}}>
+                        <div className="concept__image-wrapper">
+                            <img className="concept__image" src={this.props.image} alt="concept-image"></img>
+                        </div>
+                    </Col>
+                    <Col lg={{span:4, offset: 1}}>
+                        <div className="concept__content">
+                            <h3 className="concept__title">{this.props.title}</h3>
+                            <p className="concept__body">{this.props.body}</p>
+                        </div>
+                    </Col>
+                </Row>
+            </div>
+        );
+    }
+}
